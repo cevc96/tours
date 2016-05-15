952b8e2fee38f9fb2f95934469eb91cecbb4d8f5
davisonsp <davidsaico25@outlook.com>
2016-05-15 01:00:58 -0500

    no salio gg la laif :'v

--- application/controllers/c_main.php
+++ application/controllers/c_main.php
@@ -4,6 +4,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class C_main extends CI_Controller {
 	public function index()
 	{
-		$this->load->view('v_main');
+		$this->load->view('prueba');
 	}
 }
