0ffa438586ede26bffcc80fcdaba92bc3bb125fa
davisonsp <davidsaico25@outlook.com>
2016-05-16 01:21:21 -0500

    creo que hasta ahora esta maso mierda :v

--- application/controllers/c_main.php
+++ application/controllers/c_main.php
@@ -4,6 +4,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class C_main extends CI_Controller {
 	public function index()
 	{
+	    $this->load->view('layout/header');
 		$this->load->view('v_home');
+		$this->load->view('layout/footer');
 	}
 }
