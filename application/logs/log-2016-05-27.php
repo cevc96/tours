a4b7a1e919571593c3482f61f73890332bbd4c38
davisonsp <davidsaico25@outlook.com>
2016-05-27 11:25:45 -0500

    a

---
+++ application/logs/log-2016-05-27.php
@@ -0,0 +1,12 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2016-05-27 18:19:38 --> http://localhost:90/tours/
+ERROR - 2016-05-27 18:20:07 --> http://localhost:90/tours/
+ERROR - 2016-05-27 18:20:12 --> 404 Page Not Found: Serviceshtml/index
+ERROR - 2016-05-27 18:20:12 --> Severity: Warning --> include(C:\xampp\htdocs\tours\application\views\errors\html\error_404.php): failed to open stream: No such file or directory C:\xampp\htdocs\tours\system\core\Exceptions.php 182
+ERROR - 2016-05-27 18:20:13 --> Severity: Warning --> include(): Failed opening 'C:\xampp\htdocs\tours\application\views\errors\html\error_404.php' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\tours\system\core\Exceptions.php 182
+ERROR - 2016-05-27 18:20:14 --> http://localhost:90/tours/
+ERROR - 2016-05-27 18:20:16 --> 404 Page Not Found: Portfoliohtml/index
+ERROR - 2016-05-27 18:20:16 --> Severity: Warning --> include(C:\xampp\htdocs\tours\application\views\errors\html\error_404.php): failed to open stream: No such file or directory C:\xampp\htdocs\tours\system\core\Exceptions.php 182
+ERROR - 2016-05-27 18:20:16 --> Severity: Warning --> include(): Failed opening 'C:\xampp\htdocs\tours\application\views\errors\html\error_404.php' for inclusion (include_path='C:\xampp\php\PEAR') C:\xampp\htdocs\tours\system\core\Exceptions.php 182
+ERROR - 2016-05-27 18:20:17 --> http://localhost:90/tours/
